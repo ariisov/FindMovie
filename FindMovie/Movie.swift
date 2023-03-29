57006ec9b6dd907753a0a304ec5e85ebd37a7150
ariisov <argon328@gmail.com>
2023-03-29 23:15:33 +0300

    first commit

---
+++ FindMovie/Movie.swift
@@ -0,0 +1,8 @@
+//
+//  Movie.swift
+//  FindMovie
+//
+//  Created by Булат Хатмуллин on 29.03.2023.
+//
+
+import Foundation
---
+++ FindMovie/MovieCollectionViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  MovieCollectionViewCell.swift
+//  FindMovie
+//
+//  Created by Булат Хатмуллин on 29.03.2023.
+//
+
+import Foundation
